6ebf45a98b1160582c7e668d4be51675bad49a11
Gustavo Lima <gustavomdcl@gmail.com>
2020-03-02 19:44:08 -0300

    CRUD

--- bootcamp/modulo01/index.js
+++ bootcamp/modulo01/index.js
@@ -1,10 +1,36 @@
 const express = require('express');
 const server = express();
 
-server.get('/users/:id',(req,res)=>{ // http://localhost:3000/users/1/?nome=Gustavo
-  const {nome} = req.query;
-  const {id} = req.params;
-  return res.json({'message':`Hello ${nome} - id (${id})`});
+server.use(express.json());
+
+const users = ['Gustavo','Isabella','Mavis'];
+
+server.get('/users',(req,res)=>{
+  return res.json(users);
+});
+
+server.get('/users/:index',(req,res)=>{
+  const {index} = req.params;
+  return res.json(users[index]);
+});
+
+server.post('/users',(req,res)=>{
+  const {name} = req.body;
+  users.push(name);
+  return res.json(users);
+});
+
+server.put('/users/:index',(req,res)=>{
+  const {index} = req.params;
+  const {name} = req.body;
+  users[index] = name;
+  return res.json(users);
+});
+
+server.delete('/users/:index',(req,res)=>{
+  const {index} = req.params;
+  users.splice(index,1)
+  return res.json(users);
 });
 
 server.listen(3000);
