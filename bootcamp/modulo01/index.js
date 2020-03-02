663ed5d636fa458ce7d033b3c384bbc87d3cad88
Gustavo Lima <gustavomdcl@gmail.com>
2020-03-02 20:11:40 -0300

    Middlewares

--- bootcamp/modulo01/index.js
+++ bootcamp/modulo01/index.js
@@ -5,29 +5,53 @@ server.use(express.json());
 
 const users = ['Gustavo','Isabella','Mavis'];
 
+server.use((req,res,next)=>{
+  console.time('REQUEST');
+  console.log(`Method: ${req.method}; URL: ${req.url}`);
+  // return next();
+  next();
+  console.timeEnd('REQUEST');
+});
+function checkParamName(req,res,next){
+  if(!req.body.name){
+    return res.status(400).json({'error':'User name is required'});
+  }
+  return next();
+}
+function checkUserExists(req,res,next){
+  const user = users[req.params.index];
+  if(!user){
+    return res.status(400).json({'error':'User does not exist'});
+  }
+  req.user = user;
+  return next();
+}
+
 server.get('/users',(req,res)=>{
   return res.json(users);
 });
 
-server.get('/users/:index',(req,res)=>{
-  const {index} = req.params;
-  return res.json(users[index]);
+server.get('/users/:index',checkUserExists,(req,res)=>{
+  const {user} = req;
+  return res.json(user);
 });
 
-server.post('/users',(req,res)=>{
+server.post('/users',checkParamName,(req,res)=>{
   const {name} = req.body;
+  
   users.push(name);
+
   return res.json(users);
 });
 
-server.put('/users/:index',(req,res)=>{
+server.put('/users/:index',checkUserExists,checkParamName,(req,res)=>{
   const {index} = req.params;
   const {name} = req.body;
   users[index] = name;
   return res.json(users);
 });
 
-server.delete('/users/:index',(req,res)=>{
+server.delete('/users/:index',checkUserExists,(req,res)=>{
   const {index} = req.params;
   users.splice(index,1)
   return res.json(users);
