f6ac6508eea2d9ac40a873ea3fbdc90c9785bbd7
Gustavo Lima <gustavomdcl@gmail.com>
2020-03-04 14:55:33 -0300

    Listando agendamentos do usuário

--- bootcamp/modulo02/src/app/controllers/AppointmentController.js
+++ bootcamp/modulo02/src/app/controllers/AppointmentController.js
@@ -2,9 +2,33 @@ import * as Yup from 'yup';
 import {startOfHour,parseISO,isBefore} from 'date-fns';
 
 import User from '../models/User';
+import File from '../models/File';
 import Appointment from '../models/Appointment';
 
 class AppointmentController {
+  async index(req,res){
+    const appointments = await Appointment.findAll({
+      where:{user_id: req.userId,canceled_at: null},
+      order:['date'],
+      attributes:['id','date'],
+      include:[
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id','name'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['id','path','url']
+            }
+          ]
+        }
+      ]
+
+    });
+    return res.json(appointments);
+  }
   async store(req,res){
     const schema = Yup.object().shape({
       provider_id: Yup.number().required(),
@@ -34,7 +58,7 @@ class AppointmentController {
     if(checkAvailability) return res.status(400).json({error:'Appointment date is not available.'});
 
     const appointment = await Appointment.create({
-      user_id: req.userID,
+      user_id: req.userId,
       provider_id,
       date: hourStart
     });
