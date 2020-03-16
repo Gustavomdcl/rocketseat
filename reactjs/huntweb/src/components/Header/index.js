371e59310940c694294cb0e22220daabc7132dc0
Gustavo Lima <gustavomdcl@gmail.com>
2020-03-16 16:13:37 -0300

    React Amazenando no State

---
+++ reactjs/huntweb/src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './styles.css';
+
+const Header = ()=><header id="main-header">JSHunt</header>;
+
+// Stateless components
+// const Header = ()=>(
+//   <h1>Hello</h1>
+// );
+// ou
+// class Header extends Components() {
+//   render(){
+//     return <h1>Hello</h1>
+//   }
+// }
+
+export default Header;
