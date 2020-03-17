800f64964289dfb8d1de1642788aa31c6700440f
Gustavo Lima <gustavomdcl@gmail.com>
2020-03-17 20:24:34 -0300

    React Going

--- bootcamp/modulo05/src/pages/Main/index.js
+++ bootcamp/modulo05/src/pages/Main/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import {FaGithubAlt,FaPlus,FaSpinner} from 'react-icons/fa';
+import {Link} from 'react-router-dom';
 
-import { Container, Form, SubmitButton } from './styles';
+import Container from '../../components/Container';
+import { Form, SubmitButton, List } from './styles';
 
 import api from '../../services/api';
 
@@ -11,6 +13,18 @@ export default class Main extends Component {
     repositories: [],
     loading: false
   };
+  componentDidMount(){
+    const repositories = localStorage.getItem('repositories');
+    if(repositories){
+      this.setState({repositories: JSON.parse(repositories) });
+    }
+  }
+  componentDidUpdate(prevProp,prevState){
+    const {repositories} = this.state;
+    if(prevState.repositories!==repositories){
+      localStorage.setItem('repositories',JSON.stringify(repositories));
+    }
+  }
   handleInputChange = e => {
     this.setState({newRepo: e.target.value });
   };
@@ -29,7 +43,7 @@ export default class Main extends Component {
     });
   };
   render(){
-    const {newRepo,loading} = this.state;
+    const {newRepo,repositories,loading} = this.state;
     return (
       <Container>
         <h1>
@@ -47,6 +61,15 @@ export default class Main extends Component {
             {loading?<FaSpinner color="#fff" size={14} />:<FaPlus color="#fff" size={14} />}
           </SubmitButton>
         </Form>
+
+        <List>
+          {repositories.map(repository=>(
+            <li key={repository.name}>
+              <span>{repository.name}</span>
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>Detalhes</Link>
+            </li>
+          ))}
+        </List>
       </Container>
     );
   }
