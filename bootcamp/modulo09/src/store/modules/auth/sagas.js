544744167ac22e56665cef5cef3c415b6caae2d1
Gustavo Lima <gustavomdcl@gmail.com>
2020-03-20 13:41:27 -0300

    Requisições autenticadas

--- bootcamp/modulo09/src/store/modules/auth/sagas.js
+++ bootcamp/modulo09/src/store/modules/auth/sagas.js
@@ -26,11 +26,29 @@ export function* signIn({payload}){
 
     history.push('/dashboard');
   } catch(err){
-    toast.error('Falha na autenticação, verifique os seus dados');
+    toast.error('Falha na autenticação, verifique os seus dados!');
+    yield put(signFailure());
+  }
+}
+
+export function* signUp({payload}){
+  try{
+    const {name,email,password} = payload;
+    yield call(api.post,'users',{
+      name,
+      email,
+      password,
+      provider: true
+    });
+
+    history.push('/');
+  } catch(err){
+    toast.error('Falha no cadastro, verifique os seus dados!');
     yield put(signFailure());
   }
 }
 
 export default all([
-  takeLatest('@auth/SIGN_IN_REQUEST',signIn)
+  takeLatest('@auth/SIGN_IN_REQUEST',signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST',signUp)
 ]);
