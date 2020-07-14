c59b9bf48667e929667d8334d47a03bbc3f27497
Gustavo Lima <gustavomdcl@gmail.com>
2020-07-14 13:33:01 -0300

    Mobile - Listando projetos da API

--- gostack/nivel01/modulo03/mobile/src/index.js
+++ gostack/nivel01/modulo03/mobile/src/index.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import {View,Text,StyleSheet,StatusBar} from 'react-native';
+import React,{useEffect,useState} from 'react';
+import {View,SafeAreaView,ScrollView,FlatList,Text,StyleSheet,StatusBar} from 'react-native';
+
+import api from './services/api';
 
 // Não possuem valor semântico (significado)
 // Não possuem estilização própria
@@ -8,13 +10,36 @@ import {View,Text,StyleSheet,StatusBar} from 'react-native';
 
 // View: div, footer, header, main, aside, section
 // Text: p, span, strong, h1, h2, h3
+// FlatList: Listas
+
+// ScrollView criar scroll
 
 export default function App(){
+  const [projects,setProjects] = useState([]);
+  useEffect(()=>{
+    api.get('projects').then(response=>{
+      // console.log(response.data); // http://localhost:8081/debugger-ui/
+      setProjects(response.data);
+    });
+  },[]);
   return (<>
     <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-    <View style={styles.container}>
+
+    <SafeAreaView style={styles.container}>
+      <FlatList 
+        // style={styles.container}
+        data={projects} // Deve ser um array
+        keyExtractor={project=>project.id}
+        renderItem={({item:project})=>(<Text style={styles.project}>{project.title}</Text>)}
+      />
+    </SafeAreaView>
+
+    {/* <View style={styles.container}>
       <Text style={styles.title}>Hello GoStack</Text>
-    </View>
+      <ScrollView>
+        {projects.map(project=>(<Text key={project.id} style={styles.project}>{project.title}</Text>))}
+      </ScrollView>
+    </View> */}
   </>)
 }
 
@@ -22,12 +47,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#7159c1',
-    justifyContent: 'center',
-    alignItems: 'center',
+    // justifyContent: 'center',
+    // alignItems: 'center',
   },
   title: {
     color: '#fff',
     fontSize: 32,
     fontWeight: 'bold',
+  },
+  project: {
+    color: '#fff',
+    fontSize: 20,
   },
 });
